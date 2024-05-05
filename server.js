7c6ee1604242b1c372ac676fc234f578ebbc7278
KPhaniPrasad <37287890+KPhaniPrasad@users.noreply.github.com>
2024-05-05 17:02:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express()
 app.get('/', (req, res) => {
     res.json({
         status: 200,
-        message: "hello world testing"
+        message: "hello world testing, Yes"
     })
 })
 
